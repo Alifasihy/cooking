09ceee3c1d9d76be3b8fe55eb392de6dadc6f8ee
Alistar <alirezashr@geekology.ir>
2022-09-07 01:34:07 +0430

    .ejs extension in render function. and <%- Outputs the unescaped value into the template

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const express = require('express')
 const app = express()
-const port = process.env.port
+const port = process.env.port || 3000
 const appName= process.env.expressAppName || 'Cooking Match'
 
 app.set('views', './views')
 app.set('view-engine', 'ejs')
 
 app.get('/', (req, res) => {
-  res.render('index')
+  res.render('index.ejs')
 })
 
 app.listen(port, () => {
