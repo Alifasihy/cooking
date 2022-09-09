44456f8314637cd60508d006277d045e4dca69e8
Alistar <alirezashr@geekology.ir>
2022-09-09 23:08:29 +0430

    testing mongoose connection to datagase

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const app = express()
 const port = process.env.port || 3000
 const appName= process.env.expressAppName || 'Cooking Match'
 const path = require('path')
+
 app.use('/static', express.static(path.join(__dirname, 'public')))
+
 app.set('views', './views')
 app.set('view-engine', 'ejs')
+
+mongoose.connect('mongodb://admin@nimda:database:27017/root')
+  .then(console.log('connected'))
+  .catch(console.log('error'))
+
+
 
 app.get('/', (req, res) => {
   res.render('index.ejs')
 })
+
+
 
 app.get('/g', (req, res) => {
   res.render('gerocery.ejs')
