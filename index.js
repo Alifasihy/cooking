a6bba4713813a3092d0dac680e3cdaa81712d26d
Ali Fasihy <fasihy.ali@gmail.com>
2022-09-09 12:51:58 -0400

    add html pages

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require('express')
 const app = express()
 const port = process.env.port || 3000
 const appName= process.env.expressAppName || 'Cooking Match'
-
+const path = require('path')
+app.use('/static', express.static(path.join(__dirname, 'public')))
 app.set('views', './views')
 app.set('view-engine', 'ejs')
 
@@ -10,6 +11,9 @@ app.get('/', (req, res) => {
   res.render('index.ejs')
 })
 
+app.get('/g', (req, res) => {
+  res.render('gerocery.ejs')
+})
 app.listen(port, () => {
   console.log(`${appName} app is listening on port ${port}`)
 })
